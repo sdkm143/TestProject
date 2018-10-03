ee5aabf22ee4c3effdc415077c4a63bf2b34151b
Dharmendra.Kumar <Dharmendra.Kumar@HMECD000848.happiestminds.com>
2018-10-03 14:58:50 +0530

    comments from java side applied to the Hello

--- TestProject/src/com/test/Hello.java
+++ TestProject/src/com/test/Hello.java
@@ -1,10 +1,16 @@
 package com.test;
 
+/**
+ * @author Dharmendra.Kumar
+ *
+ */
 public class Hello {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-   System.out.println("welcome to java world with git hub ");
+		System.out.println("welcome to java world with git hub ");
 	}
 
 }
